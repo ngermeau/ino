a8868fbdd0b67c27a2c9b1480a1ac590cac6f78a
Nicolas Germeau <nicolas.germeau@gmail.com>
2021-01-31 22:28:03 +0100

    experimenting with shift register

--- remote_multiple_leds/remote_multiple_leds.ino
+++ remote_multiple_leds/remote_multiple_leds.ino
@@ -1,130 +1,22 @@
 #include "IRremote.h"
 
-int receiver = 3; // pin 1 of IR receiver to Arduino digital pin 3
-int led = 7; // pin 1 of IR receiver to Arduino digital pin 3
-
-IRrecv irrecv(receiver);           // create instance of 'irrecv'
-decode_results results;            // create instance of 'decode_results'
+int data = 9; 
+int rclock = 8; 
+int srclock = 7; 
 
 void setup()  
 {
   Serial.begin(9600);
-  irrecv.enableIRIn(); 
-  pinMode(led,OUTPUT);
+  pinMode(data,OUTPUT);     //next entry is a 0 or a 1
+  pinMode(rclock,OUTPUT);   //push data register to output
+  pinMode(srclock,OUTPUT);  //shift the register
 }
 
 void loop()   
 {
-  if (irrecv.decode(&results)) 
-
-  {
-    Serial.println(results.value, HEX);  
-    if (results.value == 0xFFA857) {
-        digitalWrite(led, HIGH); 
-    }else { 
-        digitalWrite(led, LOW); 
-    }
-    irrecv.resume(); 
-  }  
-}
-
-void translateIR() 
-
-{
-
-  switch(results.value)
-
-  {
-
-  case 0xFFA25D:  
-    Serial.println(" CH-            "); 
-    break;
-
-  case 0xFF629D:  
-    Serial.println(" CH             "); 
-    break;
-
-  case 0xFFE21D:  
-    Serial.println(" CH+            "); 
-    break;
-
-  case 0xFF22DD:  
-    Serial.println(" PREV           "); 
-    break;
-
-  case 0xFF02FD:  
-    Serial.println(" NEXT           "); 
-    break;
-
-  case 0xFFC23D:  
-    Serial.println(" PLAY/PAUSE     "); 
-    break;
-
-  case 0xFFE01F:  
-    Serial.println(" VOL-           "); 
-    break;
-
-  case 0xFFA857:  
-    Serial.println(" VOL+           "); 
-    break;
-
-  case 0xFF906F:  
-    Serial.println(" EQ             "); 
-    break;
-
-  case 0xFF6897:  
-    Serial.println(" 0              "); 
-    break;
-
-  case 0xFF9867:  
-    Serial.println(" 100+           "); 
-    break;
-
-  case 0xFFB04F:  
-    Serial.println(" 200+           "); 
-    break;
-
-  case 0xFF30CF:  
-    Serial.println(" 1              "); 
-    break;
-
-  case 0xFF18E7:  
-    Serial.println(" 2              "); 
-    break;
-
-  case 0xFF7A85:  
-    Serial.println(" 3              "); 
-    break;
-
-  case 0xFF10EF:  
-    Serial.println(" 4              "); 
-    break;
-
-  case 0xFF38C7:  
-    Serial.println(" 5              "); 
-    break;
-
-  case 0xFF5AA5:  
-    Serial.println(" 6              "); 
-    break;
-
-  case 0xFF42BD:  
-    Serial.println(" 7              "); 
-    break;
-
-  case 0xFF4AB5:  
-    Serial.println(" 8              "); 
-    break;
-
-  case 0xFF52AD:  
-    Serial.println(" 9              "); 
-    break;
-
-  default: 
-    Serial.println(" other button   ");
-
-  }
-
-  delay(500);
-
+digitalWrite(data, HIGH);
+digitalWrite(rclock, HIGH);
+delay(1000);
+digitalWrite(srclock, HIGH);
+delay(1000);
 }
